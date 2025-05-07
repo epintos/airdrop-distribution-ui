e3c65c2d64322e1f110057f53a765bf0ac71a9bd
Esteban Pintos <epintos7@gmail.com>
2025-05-07 16:11:31 +0200

    Reads allowance

--- src/components/AirdropForm.tsx
+++ src/components/AirdropForm.tsx
@@ -1,20 +1,47 @@
 "use client";
 
 import InputField from "@/components/ui/InputField";
+import { chainsToTSender, erc20Abi } from "@/constants";
+import { readContract } from "@wagmi/core";
 import { useState } from "react";
+import { useAccount, useChainId, useConfig } from "wagmi";
 
 export default function AirdropForm() {
-  const [tokenAddress, setTokenAddress ] = useState("");
+  const [tokenAddress, setTokenAddress] = useState("");
   const [recipients, setRecipients] = useState("");
   const [amounts, setAmounts] = useState("");
+  const chainId = useChainId();
+  const config = useConfig();
+  const account = useAccount();
+
+  const getApprovedAmount = async (
+    tSenderAddress: string | null
+  ): Promise<number> => {
+    if (!tSenderAddress) {
+      alert("No address found, please use a supported chain");
+      return 0;
+    }
+
+    const response = await readContract(config, {
+      abi: erc20Abi,
+      address: tokenAddress as `0x${string}`,
+      functionName: "allowance",
+      args: [account.address, tSenderAddress as `0x${string}`],
+    });
+    return response as number;
+  };
 
   const handleSubmit = async () => {
     console.log("tokenAddress", tokenAddress);
     console.log("recipients", recipients);
     console.log("amounts", amounts);
+
+    const tSenderAddress = chainsToTSender[chainId]["tsender"];
+    const approvedAmount = await getApprovedAmount(tSenderAddress);
+    console.log("approvedAmount", approvedAmount);
   };
 
-  return(
+  return (
     <div>
       <InputField
         label="Token Address"
@@ -36,9 +63,12 @@ export default function AirdropForm() {
         onChange={(e) => setAmounts(e.target.value)}
         large={true}
       />
-      <button onClick={handleSubmit}>
-        Send tokens
+      <button
+        onClick={handleSubmit}
+        className="px-4 py-2 bg-blue-600 hover:bg-blue-700 text-white font-medium rounded-lg shadow-sm transition-colors duration-200 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+      >
+        Send Tokens
       </button>
     </div>
-  )
+  );
 }
